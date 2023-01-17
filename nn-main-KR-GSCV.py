42a693853f19a9d87ec412b0eda604ec36cc7265
Jim <jeremypmoss@gmail.com>
2023-01-17 18:13:37 +1300

    Logging code to wandb.ai

--- nn-main-KR-GSCV.py
+++ nn-main-KR-GSCV.py
@@ -13,6 +13,8 @@ from tensorflow import keras
 from scikeras.wrappers import KerasRegressor
 from sklearn.metrics import mean_squared_error
 
+from wandb.keras import WandbMetricsLogger, WandbModelCheckpoint
+import wandb
 import tensorflow_docs.modeling
 import tensorflow_docs as tfdocs
 import pandas as pd
@@ -23,17 +25,19 @@ start_time = time.time()
 
 
 #%% Load data
-dataset, datasetname, magnames, mags = qf.loaddata('galexqso',
+dataset, datasetname, magnames, mags = qf.loaddata('test',
                                                    dropna = False,  # to drop NaNs
                                                    colours = True, # to compute colours of mags
                                                    impute_method = 'max') # to impute max vals for missing data
-dataset = dataset.drop(['J_mag', 'H_mag', 'K_mag'], axis = 1)
 #%% Model
+wandb.init(project = 'nn-KR-GSCV_{}'.format(datasetname))
 hyperparams = [100, 'relu', 100, 'relu', 100, 'relu']
 loss = 'mae'
 metrics = ['mae']
 epochs = 100
 opt = 'Nadam'
+
+wandb.run.log_code(".")
 
 train_frac = 0.8
 X_train, X_test, y_train, y_test = train_test_split(mags, # features
@@ -47,13 +51,15 @@ early_stop = keras.callbacks.EarlyStopping(patience=100)
 history = model.fit(X_train, y_train, epochs = epochs,
                     validation_split = 1 - train_frac,
                     verbose = 0,
-                    callbacks = [early_stop, tfdocs.modeling.EpochDots()])
+                    callbacks = [early_stop, tfdocs.modeling.EpochDots(),
+                                 WandbMetricsLogger(),
+                                 WandbModelCheckpoint("models")])
 y_pred = model.predict(X_test)
 
 X_test['z_spec'] = y_test 
 X_test['z_phot'] = y_pred
 X_test['delta_z'] = X_test['z_spec'] - X_test['z_phot']
-print("Model completed in", (time.time() - start_time)/60, "minutes")
+print("Model completed in", (time.time() - start_time), "seconds")
 
 #%% Grid search through the possible hyperparameters
 # parameters = {'loss'         : ['squared_error', 'absolute_error', 'huber', 'quantile'],
@@ -75,7 +81,7 @@ print("Model completed in", (time.time() - start_time)/60, "minutes")
 # print(grid.best_params_)
 # print(grid.best_estimator_)
 
-print("Optimisation completed in", (time.time() - start_time)/60, "minutes")
+print("Optimisation completed in", (time.time() - start_time), "seconds")
 #%% Plot results
 
 fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 9))
@@ -90,4 +96,4 @@ qf.plot_delta_z_hist(X_test['delta_z'], datasetname, model, ax = ax[1])
 qf.kurt_result(X_test['delta_z'])
 qf.plot_z_sets(y_train, X_test['z_spec'], datasetname)
 
-print("Script completed in", (time.time() - start_time)/60, "minutes")
+print("Script completed in", (time.time() - start_time), "seconds")
